2101256fdb9e27d439c74859d5cbf8f33367646d
Thomas Leese <thomas@leese.io>
2017-08-31 08:59:56 +0100

    Parse products into objects

--- tesco/tesco.py
+++ tesco/tesco.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+import re
+
 import requests
 
 
@@ -15,6 +18,32 @@ class Tesco:
         response.raise_for_status()
         return response.json()
 
+    def _camelcase_to_snakecase(self, value, do_str=False):
+        if isinstance(value, list):
+            return [self._camelcase_to_snakecase(x) for x in value]
+        elif isinstance(value, dict):
+            return {
+                self._camelcase_to_snakecase(k, True): self._camelcase_to_snakecase(v)
+                for k, v in value.items()
+            }
+        elif isinstance(value, str) and do_str:
+            s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', value)
+            return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+        else:
+            return value
+
+    def _dict_to_object(self, value):
+        if isinstance(value, list):
+            return [self._dict_to_object(x) for x in value]
+        elif isinstance(value, dict):
+            return namedtuple('NutritionObject', value.keys())(**{k: self._dict_to_object(v) for k, v in value.items()})
+        else:
+            return value
+
+    def parse_product(self, data):
+        snakecase_data = self._camelcase_to_snakecase(data)
+        return self._dict_to_object(snakecase_data)
+
     def lookup(self, gtin=None, tpnb=None, tpnc=None, catid=None):
         """Lookup a product on Tesco."""
 
@@ -25,4 +54,7 @@ class Tesco:
             'catid': catid,
         }
 
-        return self._make_request('/product/', params)['products']
+        return [
+            self.parse_product(data)
+            for data in self._make_request('/product/', params)['products']
+        ]
